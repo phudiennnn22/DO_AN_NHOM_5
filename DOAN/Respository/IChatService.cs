a95fed32bc995cc18e04ae24f1d36ec5a042c83a
vankien2909 <nhockien244@gmail.com>
2025-09-27 10:12:24 +0700

    save changes before rebase

--- DOAN/Respository/IChatService.cs
+++
@@ -1,7 +0,0 @@
-namespace DOAN.Respository
-{
-    public interface IChatService
-    {
-        Task<string> GetBotResponseAsync(string message, CancellationToken cancellationToken = default);
-    }
-} 
